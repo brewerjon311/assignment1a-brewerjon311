88a3098c21bb88e39982ca0a5dcbc73a36004a52
brewerjon311 <brewerjonathan662@gmail.com>
2024-09-11 20:11:13 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "brewerjon311";
+    char STUDENT_ID[] = "922898997";
 
     return 0 ;
 }
